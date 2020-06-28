a6256658f925c61896dffc0fd5f9cb7c2e7ba8db
Osmoday <s16762@pjwstk.edu.pl>
2020-06-28 09:44:57 +0200

    autowired the necessary repositories to the service layer

--- src/main/java/com/ipb/projekt/services/ProductManagementService.java
+++ src/main/java/com/ipb/projekt/services/ProductManagementService.java
@@ -1,7 +1,30 @@
 package com.ipb.projekt.services;
 
+import com.ipb.projekt.entities.CreatedFakturaEntity;
+import com.ipb.projekt.entities.ReceivedFakturaEntity;
+import com.ipb.projekt.repos.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductManagementService {
+
+    private final ProductRepo productRepo;
+    private final AnnexRepo annexRepo;
+    private final ClientRepo clientRepo;
+    private final ReceivedFakturaRepo receivedFakturaRepo;
+    private final CreatedFakturaRepo createdFakturaRepo;
+    private final SaleRepo saleRepo;
+    private final ShelfRepo shelfRepo;
+
+    @Autowired
+    public ProductManagementService(ProductRepo productRepo, AnnexRepo annexRepo, ClientRepo clientRepo, ReceivedFakturaRepo receivedFakturaRepo, CreatedFakturaRepo createdFakturaRepo, SaleRepo saleRepo, ShelfRepo shelfRepo) {
+        this.productRepo = productRepo;
+        this.annexRepo = annexRepo;
+        this.clientRepo = clientRepo;
+        this.receivedFakturaRepo = receivedFakturaRepo;
+        this.createdFakturaRepo = createdFakturaRepo;
+        this.saleRepo = saleRepo;
+        this.shelfRepo = shelfRepo;
+    }
 }
